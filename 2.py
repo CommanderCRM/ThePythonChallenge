72e82e2522e9938510d79bd6e9bc78c474af9b54
Ilya Krivoshein <htctomsk634@gmail.com>
2023-01-16 04:51:31 +0000

    Added second solution

---
+++ 2.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup, Comment
+from collections import Counter
+
+response = requests.get("http://www.pythonchallenge.com/pc/def/ocr.html")
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+comments = soup.find_all(string=lambda text: isinstance(text, Comment))
+needed_string = comments[1]
+counts = Counter(needed_string)
+
+print(counts)
+response.close()
