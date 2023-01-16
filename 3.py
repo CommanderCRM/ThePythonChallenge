10450eba7fab990b37e5cea85a1364d963041a99
Ilya Krivoshein <htctomsk634@gmail.com>
2023-01-16 05:10:30 +0000

    Added third solution

---
+++ 3.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup, Comment
+import re
+
+response = requests.get("http://www.pythonchallenge.com/pc/def/equality.html")
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+comments = soup.find_all(string=lambda text: isinstance(text, Comment))
+needed_string = comments[0]
+
+pattern = re.compile(r'([a-z][A-Z]{3}[a-z][A-Z]{3}[a-z])')
+matches = pattern.findall(needed_string)
+for match in matches:
+  print(match)
+
+response.close()
