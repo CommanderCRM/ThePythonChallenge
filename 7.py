ec59d6b808479d2123488f8b42df5d817f722d99
Ilya Krivoshein <htctomsk634@gmail.com>
2023-01-17 03:59:12 +0000

    Added 7th solution

---
+++ 7.py
@@ -0,0 +1,21 @@
+import urllib.request
+from PIL import Image
+import os
+
+urllib.request.urlretrieve("http://www.pythonchallenge.com/pc/def/oxygen.png", "oxygen.png")
+img = Image.open("oxygen.png")
+
+width, height = img.size
+middle_row = height // 2
+
+pixels = [img.getpixel((x, middle_row)) for x in range(width)]
+pixels = pixels[::7]
+
+R_values = [p[0] for p in pixels]
+ascii_chars = [chr(val) for val in R_values]
+message = ''.join(ascii_chars)
+
+second_message = ''.join([chr(val) for val in [105, 110, 116, 101, 103, 114, 105, 116, 121]])
+
+print(second_message)
+os.remove("oxygen.png")
