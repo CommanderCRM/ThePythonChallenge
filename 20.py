027b3a6bd919cfe2156940499ae01710832ee791
Ilya Krivoshein <htctomsk634@gmail.com>
2023-01-19 08:23:14 +0000

    Added 20th solution

---
+++ 20.py
@@ -0,0 +1,22 @@
+import urllib.request
+import base64
+import os
+from zipfile import ZipFile
+
+url = 'http://www.pythonchallenge.com/pc/hex/unreal.jpg'
+credentials = b'butter:fly'
+headers = {
+    'Authorization': 'Basic ' + base64.b64encode(credentials).decode(),
+    'Range': 'bytes=1152983631-'
+}
+
+request = urllib.request.Request(url, headers=headers)
+response = urllib.request.urlopen(request)
+
+with open("level21.zip", "wb") as f:
+    f.write(response.read())
+
+with ZipFile('level21.zip') as zf:
+    zf.extractall(pwd=b'redavni')
+
+os.remove('level21.zip')
